0faee9b7f5bbdd06523f363973d084d736183fbb
Jun Wang <jun.b.wang@oracle.com>
2022-01-23 20:17:22 -0500

    binary search

--- leetcode/leetcode/editor/en/[410]Split Array Largest Sum.py
+++ leetcode/leetcode/editor/en/[410]Split Array Largest Sum.py
@@ -45,35 +45,68 @@ import sys
 
 
 class Solution(object):
+    # def splitArray(self, nums, m):
+    #     """
+    #     :type nums: List[int]
+    #     :type m: int
+    #     :rtype: int
+    #     """
+    #     size = len(nums)
+    #     # dp[i][j]:
+    #     #   i - how many subarrays
+    #     #   j - total length of the array
+    #     dp = [[sys.maxsize] * size for _ in range(m + 1)]
+    #     sums = [0] * size
+    #     sums[0] = nums[0]
+    #     for i in range(1, size):
+    #         sums[i] = sums[i - 1] + nums[i]
+    #     for i in range(size):
+    #         dp[1][i] = sums[i]
+    #     for i in range(2, m + 1):
+    #         # print('i={}'.format(i))
+    #         # j's range needs to start from i-1 (or just 0) for the case when m==size, else
+    #         # the j loop will skip m==size
+    #         for j in range(i - 1, size):
+    #             # print('  j={}'.format(j))
+    #             for k in range(j):
+    #                 dp[i][j] = min(dp[i][j], max(dp[i - 1][k], sums[j] - sums[k]))
+    #                 # print('    dp[{}][{}]={}'.format(i, j, dp[i][j]))
+    #     return dp[m][size - 1]
+
+    # binary search
     def splitArray(self, nums, m):
         """
         :type nums: List[int]
         :type m: int
         :rtype: int
         """
-        size = len(nums)
-        # dp[i][j]:
-        #   i - how many subarrays
-        #   j - total length of the array
-        dp = [[sys.maxsize] * size for _ in range(m + 1)]
-        sums = [0] * size
-        sums[0] = nums[0]
-        for i in range(1, size):
-            sums[i] = sums[i - 1] + nums[i]
-        for i in range(size):
-            dp[1][i] = sums[i]
-        for i in range(2, m + 1):
-            # print('i={}'.format(i))
-            # j's range needs to start from i-1 (or just 0) for the case when m==size, else
-            # the j loop will skip m==size
-            for j in range(i - 1, size):
-                # print('  j={}'.format(j))
-                for k in range(j):
-                    dp[i][j] = min(dp[i][j], max(dp[i - 1][k], sums[j] - sums[k]))
-                    # print('    dp[{}][{}]={}'.format(i, j, dp[i][j]))
-        return dp[m][size - 1]
+        start = max(num for num in nums)
+        end = sum(num for num in nums)
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if self.is_valid_groups(nums, m, mid):
+                end = mid
+            else:
+                start = mid
+        is_start_valid = self.is_valid_groups(nums, m, start)
+        is_end_valid = self.is_valid_groups(nums, m, end)
+        if is_start_valid and not is_end_valid:
+            return start
+        elif not is_start_valid:
+            return end
+        else:
+            return start
+
+    def is_valid_groups(self, nums, m, target_value):
+        sum, count = 0, 1
+        for i in range(len(nums)):
+            sum += nums[i]
+            if sum > target_value:
+                sum = nums[i]
+                count += 1
+        return count <= m
 
 
 # leetcode submit region end(Prohibit modification and deletion)
-# print(Solution().splitArray([7, 2, 5, 10, 8], 2))
-print(Solution().splitArray([1, 4, 4], 3))
+print(Solution().splitArray([7, 2, 5, 10, 8], 2))
+# print(Solution().splitArray([1, 4, 4], 3))
